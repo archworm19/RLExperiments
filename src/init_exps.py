275d4842f244c08cd9774953636941d52c6161b2
Zachary Cecere <ztcecere@Zacharys-MacBook-Pro.local>
2022-11-28 15:41:28 -0500

    outline of inference system

--- src/init_exps.py
+++ src/init_exps.py
@@ -1,5 +1,6 @@
 """Temporary File for messing around with gymnasium
     Eventually: useful pieces should be factored apart"""
+from random import seed
 import numpy as np
 import numpy.random as npr
 import gymnasium as gym
@@ -13,12 +14,10 @@ def runner(env: gym.Env, action_model: Callable,
     # action_model must keep track of (memory)
     #   1. previous observations, 2. previous actions
     # action model must take in env.step output
-    _observation, _info = env.reset(seed=42)
     action = init_action
     for _ in range(max_step):
         step_output = env.step(action)
         action = action_model(step_output)
-    env.close()
 
 
 # Policies
@@ -113,7 +112,13 @@ def simple_control_lunar(step_output: Tuple, rng: npr.Generator):
     theta_adj = theta + (np.pi / 2.)
     v_dir = np.array([np.cos(theta_adj), np.sin(theta_adj)])
     v_adj = v_diff - v_dir
-    if rng.random() < 0.35:
+
+    # sampling: up vs. side thrust
+    up_mag = 1.5 - y
+    theta_mag = np.fabs(v_adj[0])
+    theta_prob = theta_mag / (theta_mag + up_mag)
+
+    if rng.random() < theta_prob:
         # left booster:
         if v_adj[0] > 0:
             return 3
@@ -121,9 +126,73 @@ def simple_control_lunar(step_output: Tuple, rng: npr.Generator):
     return 2
 
 
+def lunar_control_look(step_output: Tuple, rng: npr.Generator,
+                       k3: float = 0.1):
+    # TODO: add paramter for t --> how far ahead to predict
+
+    # simple lunar model with look-ahead projection
+    # Taylor Series approximation
+    #   theta(t) = theta(t0) + k1 * t + k2/2 * t^2
+    # Apply this approximation at each timestep + assume k2
+    #       constant assumption = constant force from thrust
+    # additional assumptions:
+    #   1. work in t-steps = 1
+    #   2. k1 given by model's dtheta
+    # --> theta(t+1) = theta + dtheta + k3
+    # where we'll infer k3
+    x_targ = 0.
+    y_targ = 5.
+
+    obs = step_output[0]
+    x = obs[0]
+    y = obs[1]
+    # stopgap
+    if y > 1.4:
+        return 0
+    theta = obs[4]
+    dtheta = obs[5]
+
+    # vector comparison
+    v_diff = np.array([x_targ - x, y_targ - y])
+    theta_adj = theta + (np.pi / 2.)
+
+    # Counterfactual
+    # predict new theta as a function of different thrusts
+    #   choose the option with the lower v_adj x magnitude
+    actions = (1, 3)
+    scores = []
+    for k_force in (k3, -k3):
+        theta_taylor = theta_adj + dtheta + k_force
+        v_dir = np.array([np.cos(theta_taylor), np.sin(theta_taylor)])
+        v_adj = v_diff - v_dir
+        scores.append(v_adj[0])
+
+    if rng.random() < 0.35:
+        if np.fabs(scores[0]) < np.fabs(scores[1]):
+            return actions[0]
+        return actions[1]
+    return 2
+
+
+# Full On Inference system!
+# improvement on above system:
+# project x_new, y_new as well as theta_new
+# > for each thrust possibility
+# > > use taylor series to project new vector = v_new
+# > take thrust option that minimizes f(v_new, v_target)
+# ... need a good choice for f (probably just use a weighted average to start)
+
+
 if __name__ == "__main__":
     # lunar lander
     env = gym.make("LunarLander-v2", render_mode="human")
-    # runner(env, partial(semi_random_policy, rng = npr.default_rng(42)))
-    runner(env, partial(simple_control_lunar, rng = npr.default_rng(42)),
-           max_step=1000)
+    #env.reset(seed=42)
+    #runner(env, partial(semi_random_policy, rng = npr.default_rng(42)))
+    #env.reset(seed=42)
+    #runner(env, partial(simple_control_lunar, rng = npr.default_rng(42)),
+    #        max_step=500)
+    for k in [.005, .01]:
+        env.reset(seed=42)
+        runner(env, partial(lunar_control_look, rng = npr.default_rng(42), k3=k),
+                max_step=1000)
+    env.close()
