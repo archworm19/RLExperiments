cea9e31562a15853abd001171781d0fcac752aec
Zachary Cecere <ztcecere@Zacharys-MacBook-Pro.local>
2022-12-03 16:05:15 -0500

    rdy to test double q agent

--- src/agents/q_agents.py
+++ src/agents/q_agents.py
@@ -11,24 +11,30 @@ import tensorflow as tf
 from typing import List
 from tensorflow.keras.layers import Layer
 from frameworks.agent import Agent, RunData
-from frameworks.q_learning import QLearning
+from frameworks.q_learning import calc_q_error_sm
 from frameworks.custom_model import CustomModel
 from agents.utils import build_action_probes
 
 
 class QAgent(Agent):
+    # double DQN
+    # stateful: switches active model at the end of each train call
 
     def __init__(self,
-                 scalar_model: Layer,
+                 model0: Layer,
+                 model1: Layer,
                  num_actions: int,
                  state_dims: int,
                  gamma: float = 0.95,
                  rand_act_prob: float = 0.25):
         """
         Args:
-            scalar_model (Layer): keras layer where the call
-                has the following signature:
-                    call(action_t: )
+            model0 (Layer):
+            model1 (Layer):
+                scalar_models
+                keras layers with the following call signature
+                    call(action_t: tf.Tensor, state_t: List[tf.Tensor])
+                        --> tf.Tensor (with shape = batch_size)
             num_actions (int): number of actions available to
                 the agent
             state_dims (int): number of dimensions in state
@@ -37,14 +43,15 @@ class QAgent(Agent):
             gamma (float): discount factor
         """
         super(QAgent, self).__init__()
-        self.scalar_model = scalar_model
+        self.model0 = model0
+        self.model1 = model1
         self.num_actions = num_actions
         self.rand_act_prob = rand_act_prob
         self.gamma = gamma
         self.rng = npr.default_rng(42)
+        self.active_model_idx = 0
+        self.modelz = [self.model0, self.model1]
 
-        # TODO: is this really the right place
-        # to build the custom model?
         inputs = [tf.keras.Input(shape=(num_actions,),
                                  name="action", dtype=tf.float32),
                   tf.keras.Input(shape=(),
@@ -52,30 +59,29 @@ class QAgent(Agent):
                   tf.keras.Input(shape=(state_dims,),
                                  name="state", dtype=tf.float32),
                   tf.keras.Input(shape=(state_dims,),
-                                 name="state_t1", dtype=tf.float32)]
-        Qerr, Y_t, Q_t = QLearning(gamma).calc_error(num_actions,
-                                                     scalar_model,
-                                                     inputs[1],
-                                                     inputs[0],
-                                                     [inputs[2]],
-                                                     [inputs[3]])
-        outputs = {"loss": tf.math.reduce_mean(Qerr),
-                   "Qerr": Qerr,
-                   "Y_t": Y_t,
-                   "Q_t": Q_t}
-        self.kmodel = CustomModel("loss",
+                                 name="state_t1", dtype=tf.float32),]
+        # need to duplicate losses and models to be able to switch
+        Q_err0, _ = calc_q_error_sm(self.model0, self.model1,
+                                    inputs[0], inputs[1], inputs[2], inputs[2],
+                                    self.num_actions, self.gamma)
+        Q_err1, _ = calc_q_error_sm(self.model1, self.model0,
+                                    inputs[0], inputs[1], inputs[2], inputs[2],
+                                    self.num_actions, self.gamma)        
+        self.kmodel0 = CustomModel("loss",
                                   inputs=inputs,
-                                  outputs=outputs)
-        self.kmodel.compile(tf.keras.optimizers.SGD(learning_rate=0.001,
-                                                    momentum=0.0,
-                                                    nesterov=False,
-                                                    name="SGD"))
+                                  outputs={"loss": tf.math.reduce_mean(Q_err0)})
+        self.kmodel0.compile(tf.keras.optimizers.RMSprop(.01))
+        self.kmodel1 = CustomModel("loss",
+                                  inputs=inputs,
+                                  outputs={"loss": tf.math.reduce_mean(Q_err1)})
+        self.kmodel1.compile(tf.keras.optimizers.RMSprop(.01))
+        self.kmodelz = [self.kmodel0, self.kmodel1]
+
 
     def init_action(self):
         return self.rng.integers(0, self.num_actions)
 
     def select_action(self, state: List[np.ndarray], debug: bool = False):
-        # TODO: missing randomness!!!
         """greedy action selection
 
         Args:
@@ -94,7 +100,7 @@ class QAgent(Agent):
         # --> state_t = num_actions x ...
         action_t, state_t = build_action_probes(state, self.num_actions)
         # --> shape = num_actions
-        scores = self.scalar_model(action_t, state_t)
+        scores = self.modelz[self.active_model_idx](action_t, state_t)
 
         if debug:
             print('action; state; scores')
@@ -113,7 +119,7 @@ class QAgent(Agent):
                 "state": run_data.states,
                 "state_t1": run_data.states_t1}
         dset = tf.data.Dataset.from_tensor_slices(dmap)
-        return dset.shuffle(1000000).batch(256)
+        return dset.shuffle(1000000).batch(128)
 
     def train(self, run_data: RunData, num_epoch: int, debug: bool = False):
         """train agent on run data
@@ -123,18 +129,8 @@ class QAgent(Agent):
             num_epoch (int):
         """
         dset = self._build_dset(run_data)
-
-        if debug:
-            input("DEBUG")
-            for i, v in enumerate(dset):
-                if i >= 10:
-                    break
-                print(v["reward"][:5])
-                vout = self.kmodel(v)
-                print(vout["Y_t"][:5])
-                print(vout["Q_t"][:5])
-                input("cont?")
-
-        history = self.kmodel.fit(dset,
-                                  epochs=num_epoch)
+        history = self.kmodelz[self.active_model_idx].fit(dset,
+                                                          epochs=num_epoch)
+        # stateful
+        self.active_model_idx = 1 - self.active_model_idx
         return history
