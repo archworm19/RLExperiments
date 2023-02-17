9766a64bb61a5077ea6a7973e6f71968275403bb
Zachary Cecere <ztcecere@Zacharys-MacBook-Pro.local>
2023-02-17 13:04:00 -0500

    queue system is best

--- src/run_scripts/ppo_train.py
+++ src/run_scripts/ppo_train.py
@@ -1,18 +1,12 @@
 """PPO"""
-import os
+import pickle
 import numpy as np
 import numpy.random as npr
 from typing import Callable
-from multiprocessing import connection, Process, Pipe
+from multiprocessing import connection, Process, Pipe, Queue
 from functools import partial
 from run_scripts.runner import simple_run
 from run_scripts.builders import EnvsDiscrete, EnvsContinuous, build_discrete_ppo, build_continuous_ppo
-
-
-def _one_hot(x: np.ndarray, num_action: int):
-    # --> T x num_action
-    v = [1. * (x == i) for i in range(num_action)]
-    return np.concatenate([vi[:, None] for vi in v], axis=1)
 
 
 def run_and_train(env_run, env_viz, agent,
@@ -49,102 +43,62 @@ def run_and_train(env_run, env_viz, agent,
         print(np.sum(reward))
 
 
-# TODO: design for parallelism?
-# > child vs. parent process?
-#   parent train; children run
-#   children copy parent model
-# > model sharing
-#   use keras save/load
-#   interface?
-#       save_model()
-#       load_model(root_directory: str)
-#   if there are multiple models --> model itself handles the internals (i.e. make subdirectories for diff models)
-# > we need to make different keras models for ppo (forward/no_train model)
-# > TODO: might be better/more efficient to just share weights... cuz graph will remain the same!!
-# > data sharing
-#   LAZY soln (no generator and potentially inefficient data storage)
-#   save each field output of simple run as npz
-#       Ex: reward = npz where different arrays are different trajectories
-#   parent loads and passes into train
-# > parallelism
-#   start simple: just use pool
-#       pass in model location directory to each process
-#       get back the locations of the saved data
-#   TODO: if we're sharing weights --> better to keep static pool of processes
-#       
-# > delete old saved data
-
-
-def _ll_run(conn: connection.Connection,
-            builder: Callable,  # --> env_run and agent
-            process_id: int,
-            weight_directory: str,
-            data_directory: str,
-            T_run: int,
-            run_cutoff: int,
-            rand_seed: int,
-            discrete_mode: bool = True):
+def _ll_run_queue(conn: connection.Connection,
+                  queue: Queue,
+                  builder: Callable,  # --> env_run and agent
+                  weight_directory: str,
+                  T_run: int,
+                  run_cutoff: int,
+                  rand_seed: int,
+                  discrete_mode: bool = True):
     # process function:
     # > builds
-    # > waits for signal --> load weights from directory --> saves run data
+    # > waits for signal --> load weights from directory --> adds run data to queue
+    # how to tell parent that child is done? send empty dictionary
+    #       real data sent as pickle(Dict[str, np.ndarray])
     rng = npr.default_rng(rand_seed)
     env_run, _, agent = builder()
 
     while True:
         run_sig = conn.recv()
-
         if not run_sig:
             break
-
         agent.load_weights(weight_directory)
-        sv, av, rv, tv = [], [], [], []
         num_step = 0
         while(num_step < T_run):
             env_run.reset(seed=int(rng.integers(10000)))
             states, actions, rewards, term = simple_run(env_run, agent, run_cutoff, debug=False, discrete=discrete_mode)
-            sv.append(np.array(states))
-            av.append(np.array(actions))
-            rv.append(np.array(rewards))
-            tv.append(term)
-            num_step += np.shape(rv[-1])[0]
-        # overwrite old data:  NOTE: this is dangerous
-        np.savez(os.path.join(data_directory, "states{0}.npz".format(process_id)), *sv)
-        np.savez(os.path.join(data_directory, "actions{0}.npz").format(process_id), *av)
-        np.savez(os.path.join(data_directory, "rewards{0}.npz").format(process_id), *rv)
-        np.save(os.path.join(data_directory, "termination{0}.npy").format(process_id), np.array(tv))
-        conn.send(True)  # tell parent that run is finished
+            num_step += np.shape(rewards)[0]
+
+            # send current trajectory through the queue
+            d = {"states": np.array(states), "actions": np.array(actions),
+                 "rewards": np.array(rewards), "term": term}
+            queue.put(pickle.dumps(d), block=True)            
+        # finished signal = empty dict thru queue:
+        queue.put(pickle.dumps({}), block=True)
 
 
-def _load_npz_helper(rdir: str, fn: str, pid: int):
-    d = np.load(os.path.join(rdir, "{0}{1}.npz".format(fn, pid)))
-    l = [d["arr_"+str(i)] for i in range(len(d))]
-    return l
-
-
-# TODO: experiment with transmitting data over a queue
-
-def ll_run_and_train(builder: Callable,  # --> env_run, env_viz, agent
-                     num_procs: int,
-                     weight_directory: str,
-                     data_directory: str,
-                     num_epoch: int,
-                     T_run: int,
-                     T_test: int,
-                     run_cutoff: int,
-                     rand_seed: int,
-                     discrete_mode: bool = True,
-                     viz_debug: bool = False):
+def ll_run_and_train_queue(builder: Callable,  # --> env_run, env_viz, agent
+                           num_procs: int,
+                           weight_directory: str,
+                           num_epoch: int,
+                           T_run: int,  # per process
+                           T_test: int,
+                           run_cutoff: int,
+                           rand_seed: int,
+                           discrete_mode: bool = True,
+                           viz_debug: bool = False):
     rng = npr.default_rng(rand_seed)
     # set up the processes:
     procs, conns = [], []
+    q = Queue(maxsize=100)  # TODO: meaning of maxsize? (in bytes? in elems?)
     for i in range(num_procs):
         parent_conn, child_conn = Pipe()
-        func_target = partial(_ll_run,
+        func_target = partial(_ll_run_queue,
                               conn=child_conn,
+                              queue=q,
                               builder=builder,
-                              process_id=i,
                               weight_directory=weight_directory,
-                              data_directory=data_directory,
                               T_run=T_run,
                               run_cutoff=run_cutoff,
                               rand_seed=i,
@@ -164,22 +118,26 @@ def ll_run_and_train(builder: Callable,  # --> env_run, env_viz, agent
         # run
         for conn in conns:
             conn.send(True)  # means "run it"
-        # block till all processes complete:
-        for conn in conns:
-            conn.recv()
 
+        # pull from queue till all processes have sent stop signal
+        num_stops = 0
         sv, av, rv, tv = [], [], [], []
-        # load data from each child:
-        for i in range(num_procs):
-            sv.extend(_load_npz_helper(data_directory, "states", i))
-            av.extend(_load_npz_helper(data_directory, "actions", i))
-            rv.extend(_load_npz_helper(data_directory, "rewards", i))
-            tv.extend(np.load(os.path.join(data_directory, "termination{0}.npy").format(i)).tolist())
+        while num_stops < num_procs:
+            d = pickle.loads(q.get(block=True))
+            # check for finished signal
+            if len(d) < 1:
+                num_stops += 1
+                continue
+            sv.append(d["states"])
+            av.append(d["actions"])
+            rv.append(d["rewards"])
+            tv.append(d["term"])
+
         # train and test:
         agent.train([{"core_state": si} for si in sv],
                     rv,
                     av,
-                    tv)
+                    np.array(tv))
 
         # save agent weights
         agent.save_weights(weight_directory)
@@ -214,6 +172,6 @@ if __name__ == "__main__":
     # run_and_train(env_run, env_viz, agent, num_actions, 20, 20000, 500, 500, viz_debug=False, discrete_mode=discrete_mode)
 
 
-    # parallel
+    # parallel + queue
     builder = partial(build_discrete_ppo, env=EnvsDiscrete.cartpole)
-    ll_run_and_train(builder, 2, 'weights/', 'data/', 10, 5000, 500, 500, 42, discrete_mode=True)
+    ll_run_and_train_queue(builder, 1, 'weights/', 6, int(4*10000), 500, 500, 42, discrete_mode=True)
