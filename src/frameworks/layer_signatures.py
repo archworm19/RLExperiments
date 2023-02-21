3032a12fbde80cc757fb29a01fd6bd427eb680d3
Zachary Cecere <ztcecere@Zacharys-MacBook-Pro.local>
2023-02-21 14:40:11 -0500

    new signatures integrated into q learning

--- src/frameworks/layer_signatures.py
+++ src/frameworks/layer_signatures.py
@@ -1,16 +1,30 @@
-"""Keras Layer Signatures ~ recommend more explicit call signatures
-    than standard keras"""
+"""Keras Layer Signature wrappers
+
+    # > test tensor design
+    #       Sigs are not layers ~ they encapsulate layer
+    #       __call__(...) Returns 2 things
+    #           1. layer.call(...) -> target tensor of interest
+    #           2. bool tensor = logical_and of all test cases
+    #       How to use?
+    #           make a test version of model --> run test on first train...
+    #       Q? does this add a bunch of overhead?
+    #           It shouldn't... cuz main model doesn't use the test tensors
+    #        
+
+    """
 import tensorflow as tf
-from typing import List
+from typing import List, Tuple
 from tensorflow.keras.layers import Layer
 
 
-class ScalarModel(Layer):
+class ScalarModel():
+    # layer must implement
+    #   action_t + state_t --> scalar (shape = batch_size)
 
-    def __init__(self):
-        super(ScalarModel, self).__init__()
+    def __init__(self, layer: Layer):
+        self.layer = layer
 
-    def call(self, action_t: tf.Tensor, state_t: List[tf.Tensor]) -> tf.Tensor:
+    def __call__(self, action_t: tf.Tensor, state_t: List[tf.Tensor]) -> Tuple[tf.Tensor, tf.Tensor]:
         """
         Args:
             action_t (tf.Tensor): action
@@ -21,16 +35,21 @@ class ScalarModel(Layer):
 
         Returns:
             tf.Tensor: shape = batch_size
+            tf.Tensor: single boolean ~ true if tests pass
         """
-        pass
+        v = self.layer(action_t, state_t)
+        v_test = tf.math.reduce_all(tf.shape(tf.shape(v)) == 1)
+        return v, v_test
 
 
-class ScalarStateModel(Layer):
+class ScalarStateModel():
+    # layer must implement
+    #   state_t --> scalar (shape = batch_size)
 
-    def __init__(self):
-        super(ScalarStateModel, self).__init__()
+    def __init__(self, layer: Layer):
+        self.layer = layer
 
-    def call(self, state_t: List[tf.Tensor]) -> tf.Tensor:
+    def __call__(self, state_t: List[tf.Tensor]) -> Tuple[tf.Tensor, tf.Tensor]:
         """
         Args:
             state_t (List[tf.Tensor]): set of states
@@ -39,17 +58,22 @@ class ScalarStateModel(Layer):
 
         Returns:
             tf.Tensor: shape = batch_size
+            tf.Tensor: single boolean ~ true if tests pass
         """
-        pass
+        v = self.layer(state_t)
+        v_test = tf.math.reduce_all(tf.shape(tf.shape(v)) == 1)
+        return v, v_test
 
 
-class DistroModel(Layer):
+class DistroModel():
     # distributional model
+    # layer must implement:
+    #   action_t + state_t --> batch_size x d (normalized across d)
 
-    def __init__(self):
-        super(DistroModel, self).__init__()
+    def __init__(self, layer: Layer):
+        self.layer = layer
 
-    def call(self, action_t: tf.Tensor, state_t: List[tf.Tensor]) -> tf.Tensor:
+    def __call__(self, action_t: tf.Tensor, state_t: List[tf.Tensor]) -> Tuple[tf.Tensor, tf.Tensor]:
         """
         Args:
             action_t (tf.Tensor): action
@@ -61,17 +85,21 @@ class DistroModel(Layer):
         Returns:
             tf.Tensor: shape = batch_size x d
                 where d = number of elements representing distribution
+            tf.Tensor: single boolean ~ true if tests pass
         """
-        pass
+        v = self.layer(action_t, state_t)
+        v_test = tf.math.logical_and(tf.math.reduce_all(tf.shape(tf.shape(v)) == 2),
+                                     tf.math.reduce_all(tf.math.abs(tf.math.reduce_sum(v, axis=1) - 1.) < .001))
+        return v, v_test
 
 
-class DistroStateModel(Layer):
+class DistroStateModel():
     # distributional model
 
-    def __init__(self):
-        super(DistroStateModel, self).__init__()
+    def __init__(self, layer: Layer):
+        self.layer = layer
 
-    def call(self, state_t: List[tf.Tensor]) -> tf.Tensor:
+    def __call__(self, state_t: List[tf.Tensor]) -> Tuple[tf.Tensor, tf.Tensor]:
         """
         Args:
             state_t (List[tf.Tensor]): set of states
@@ -81,5 +109,9 @@ class DistroStateModel(Layer):
         Returns:
             tf.Tensor: shape = batch_size x d
                 where d = number of elements representing distribution
+            tf.Tensor: single boolean ~ true if tests pass
         """
-        pass
+        v = self.layer(state_t)
+        v_test = tf.math.logical_and(tf.math.reduce_all(tf.shape(tf.shape(v)) == 2),
+                                     tf.math.reduce_all(tf.math.abs(tf.math.reduce_sum(v, axis=1) - 1.) < .001))
+        return v, v_test
